3ff380eade941351b5de63de05ca56e9a7f7d3f3
yuji.yamamoto <yamamoto@ridgeroot.com>
2015-10-24 10:17:22 +0900

    add project base files

---
+++ FollowList/FollowList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FollowList
+//
+//  Created by 山本 裕司 on 2015/10/20.
+//  Copyright © 2015年 ridgeroot co.ltd.,. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
